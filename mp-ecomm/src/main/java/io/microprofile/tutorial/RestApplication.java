3b7811ead60859c455394f4b9331bfd64da154d6
Tarun Telang <tarun.telang@gmail.com>
2024-02-23 03:47:02 +0530

    Delete mp-ecomm directory

--- mp-ecomm/src/main/java/io/microprofile/tutorial/RestApplication.java
+++
@@ -1,9 +0,0 @@
-package io.microprofile.tutorial;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class RestApplication extends Application {
-    
-}
